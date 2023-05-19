e5922dd9ee0fa02d2c6598027dff0cc225913fae
Craig Kubinec <craig.kubinec@gmail.com>
2023-05-19 12:54:56 -0600

    Added More Routing

---
+++ app/(site)/woman/page.tsx
@@ -0,0 +1,27 @@
+import FirstCustomerSee from "../../components/UI/FirstCustomerSee";
+import ScrollingLargeTiles from "../../components/UI/ScrollingLargeTiles";
+import ScrollingSmallTiles from "../../components/UI/ScrollingSmallTiles";
+const WomanPage = () => {
+  return (
+    <div>
+      <FirstCustomerSee />
+      <ScrollingLargeTiles />
+      <ScrollingSmallTiles />
+      <div>
+        <h2>WOMANS'S CLOTHING & SHOES</h2>
+        <p>
+          In sport and in life, creators aren’t content on the sidelines. adidas
+          women’s shoes and apparel are made for those who understand that rules
+          can be negotiated; expectations, evolved. Reach for a new personal
+          best in apparel made of specialized performance fabrics. Accentuate
+          your personal style in sport-inspired sneakers for your day-to-day.
+          Whether sport is your life or you’re an athleisure fashionista,
+          women’s clothing and footwear from adidas exist to help you redefine
+          what’s possible.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default WomanPage;
